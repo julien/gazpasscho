6c015c3e614b50407e47aad91f8bb9fae868261d
Carlos Lancha <carlos.lancha@liferay.com>
2017-06-09 14:38:05 -0700

    Create passwords page

---
+++ src/pages/Passwords.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+
+import templates from './Passwords.soy';
+
+class Passwords extends Component {
+}
+
+Soy.register(Passwords, templates);
+
+export default Passwords;
