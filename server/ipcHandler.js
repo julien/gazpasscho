607571366c72b7fa5caae735f23a38d0af1ac1dc
drewbrokke <drew.brokke@liferay.com>
2017-06-09 15:05:35 -0700

    ipcHandler.js - sends return messages to the renderer when actions have completed

--- server/ipcHandler.js
+++ server/ipcHandler.js
@@ -2,30 +2,37 @@ const ipcMain = require('electron').ipcMain;
 const api = require('./api');
 
 ipcMain.on('createEntry', (event, { title, userName, password, url }) => {
-	api.createEntry(title, userName, password, url);
+	api.createEntry(title, userName, password, url)
+		then(entry => event.sender.send('entryCreated', entry));
 });
 
 ipcMain.on('deleteEntry', (event, { id }) => {
-	api.deleteEntry(id);
+	api.deleteEntry(id)
+		.then(() => api.getAllEntries())
+		.then(entries => event.sender.send('entryDeleted', entries));
 });
 
 ipcMain.on('getAllEntries', event => {
-	api.getAllEntries();
+	api.getAllEntries()
+		.then(entries => event.sender.send('allEntriesRetrieved', entries));
 });
 
 ipcMain.on('updateEntry', (event, { id, data }) => {
-	api.updateEntry(id, data);
+	api.updateEntry(id, data)
+		.then(entry => event.sender.send('entryUpdated', entry));
 });
 
 ipcMain.on('search', (event, { keywords }) => {
-	api.search(keywords);
+	api.search(keywords)
+		then(entries => event.sender.send('entriesFound', entries));
 });
 
 ipcMain.on('open', (event, { databaseFile, databasePassword }) => {
-	api.open(databaseFile, databasePassword);
-	event.sender.send('databaseOpened');
+	api.open(databaseFile, databasePassword)
+		then(() => event.sender.send('databaseOpened'));
 });
 
 ipcMain.on('close', event => {
-	api.close();
+	api.close()
+		then(() => event.sender.send('databaseClosed'));
 });
