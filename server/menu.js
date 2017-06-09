aafa21f8c577efa09e5d632d31763aab88d4683a
drewbrokke <drew.brokke@liferay.com>
2017-06-09 11:02:31 -0700

    menu.js - Adds base custom menu items

--- server/menu.js
+++ server/menu.js
@@ -1,13 +1,53 @@
 const electron = require("electron");
 
+const app = electron.app;
+
 const Menu = electron.Menu;
+const MenuItem = electron.MenuItem;
 
 let menu;
 
+const appMenuItem = new MenuItem({
+  label: app.getName(),
+  submenu: [
+    { role: "about" },
+    { type: "separator" },
+    { role: "services", submenu: [] },
+    { type: "separator" },
+    { role: "hide" },
+    { role: "hideothers" },
+    { role: "unhide" },
+    { type: "separator" },
+    { role: "quit" }
+  ]
+});
+
+const viewMenuItem = new MenuItem({
+  label: "View",
+  submenu: [
+    { role: "reload" },
+    { role: "forcereload" },
+    { role: "toggledevtools" },
+    { type: "separator" },
+    { role: "resetzoom" },
+    { role: "zoomin" },
+    { role: "zoomout" },
+    { type: "separator" },
+    { role: "togglefullscreen" }
+  ]
+});
+
+const windowMenuItem = new MenuItem({
+  role: "window",
+  submenu: [{ role: "minimize" }, { role: "close" }]
+});
+
 module.exports = function createMenu() {
-	menu = new Menu();
+  menu = new Menu();
 
-	Menu.setApplicationMenu(menu);
+  menu.append(appMenuItem);
+  menu.append(viewMenuItem);
+  menu.append(windowMenuItem);
 
-	console.log(menu);
+  Menu.setApplicationMenu(menu);
 };
