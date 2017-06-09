79c8b3f4e97c441d013488f2d060de0319f1160d
Carlos Lancha <carlos.lancha@liferay.com>
2017-06-09 13:53:19 -0700

    Add select database file form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,30 @@
 
 import Component from 'metal-component';
 import Soy from 'metal-soy';
+const {ipcRenderer} = require('electron')
 
 import templates from './Home.soy';
 
-class Home extends Component {}
+class Home extends Component {
+	_submitHandler(event) {
+		event.preventDefault();
+
+		let form = event.delegateTarget;
+
+		let databaseFile = form.querySelector('#databaseFile').value;
+
+		let databasePassword = form.querySelector('#databasePassword').value;
+
+		ipcRenderer.send(
+			'open',
+			{
+				databaseFile: databaseFile,
+				databasePassword: databasePassword
+			}
+		);
+	}
+}
+
 Soy.register(Home, templates);
 
 export default Home;
