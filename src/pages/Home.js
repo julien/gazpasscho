d654b51f17523576dd5f3d4e8cd9279480d6498b
Carlos Lancha <carlos.lancha@liferay.com>
2017-06-09 14:30:29 -0700

    Use electron dialog for files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,24 @@
 
 import Component from 'metal-component';
 import Soy from 'metal-soy';
-const {ipcRenderer} = require('electron')
+const {ipcRenderer} = require('electron');
+const {dialog} = require('electron').remote;
 
 import templates from './Home.soy';
 
 class Home extends Component {
+	_selectFileHandler(event) {
+		let self = this;
+		dialog.showOpenDialog((fileNames) => {
+
+		    if(fileNames === undefined){
+		        return;
+		    }
+
+		    self.element.querySelector('#databaseFile').value = fileNames[0];
+		});
+	}
+
 	_submitHandler(event) {
 		event.preventDefault();
 
