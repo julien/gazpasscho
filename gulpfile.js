ee9910120b0909a93a968c7000828ec922fbdd06
Iván Zaera Avellón <izaera@gmail.com>
2017-06-09 16:59:38 -0700

    Copy more things related to fonts

--- gulpfile.js
+++ gulpfile.js
@@ -13,5 +13,8 @@ metal.registerTasks({
 gulp.task('build', ['me:build'], () => {
 	gulp
 		.src('node_modules/lexicon-ux/src/fonts/alloy-font-awesome/font/**/*')
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest('build/fonts/alloy-font-awesome/font/'));
+	gulp
+		.src('node_modules/lexicon-ux/build/css/font-awesome.css')
+		.pipe(gulp.dest('build/css'));
 });
