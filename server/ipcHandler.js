9f3398033b6fcdc3eb557ed20e52ba8ebfb46fb9
drewbrokke <drew.brokke@liferay.com>
2017-06-09 15:07:08 -0700

    ipcHandler.js - adds catch statements

--- server/ipcHandler.js
+++ server/ipcHandler.js
@@ -3,36 +3,43 @@ const api = require('./api');
 
 ipcMain.on('createEntry', (event, { title, userName, password, url }) => {
 	api.createEntry(title, userName, password, url)
-		then(entry => event.sender.send('entryCreated', entry));
+		then(entry => event.sender.send('entryCreated', entry))
+		.catch(error => event.sender.send('error', error));
 });
 
 ipcMain.on('deleteEntry', (event, { id }) => {
 	api.deleteEntry(id)
 		.then(() => api.getAllEntries())
-		.then(entries => event.sender.send('entryDeleted', entries));
+		.then(entries => event.sender.send('entryDeleted', entries))
+		.catch(error => event.sender.send('error', error));
 });
 
 ipcMain.on('getAllEntries', event => {
 	api.getAllEntries()
-		.then(entries => event.sender.send('allEntriesRetrieved', entries));
+		.then(entries => event.sender.send('allEntriesRetrieved', entries))
+		.catch(error => event.sender.send('error', error));
 });
 
 ipcMain.on('updateEntry', (event, { id, data }) => {
 	api.updateEntry(id, data)
-		.then(entry => event.sender.send('entryUpdated', entry));
+		.then(entry => event.sender.send('entryUpdated', entry))
+		.catch(error => event.sender.send('error', error));
 });
 
 ipcMain.on('search', (event, { keywords }) => {
 	api.search(keywords)
-		then(entries => event.sender.send('entriesFound', entries));
+		then(entries => event.sender.send('entriesFound', entries))
+		.catch(error => event.sender.send('error', error));
 });
 
 ipcMain.on('open', (event, { databaseFile, databasePassword }) => {
 	api.open(databaseFile, databasePassword)
-		then(() => event.sender.send('databaseOpened'));
+		then(() => event.sender.send('databaseOpened'))
+		.catch(error => event.sender.send('error', error));
 });
 
 ipcMain.on('close', event => {
 	api.close()
-		then(() => event.sender.send('databaseClosed'));
+		then(() => event.sender.send('databaseClosed'))
+		.catch(error => event.sender.send('error', error));
 });
