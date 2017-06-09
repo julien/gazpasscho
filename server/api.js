d5079e34926ab2514273631369b34f660f466246
drewbrokke <drew.brokke@liferay.com>
2017-06-09 14:49:10 -0700

    api.js - implements a couple of db methods and error checking

--- server/api.js
+++ server/api.js
@@ -1,36 +1,77 @@
-function createEntry(data) {
+const {Database, Entry} = require('./database');
+
+let db;
+
+function createEntry(title, userName, password, url) {
 	console.log('createEntry');
-	console.log('data: ', data);
+	console.log('title: ', title);
+	console.log('username: ', userName);
+	console.log('password: ', password);
+	console.log('url: ', url);
+
+	if (!db) return Promise.reject(new Error('Database is not initialized.'));
+	if (!title) return Promise.reject(new Error('A title is required.'));
+	if (!userName) return Promise.reject(new Error('A username is required.'));
+	if (!password) return Promise.reject(new Error('A password is required.'));
+	if (!url) return Promise.reject(new Error('A url is required.'));
+
+	const newEntry = db.addEntry({title, userName, password, url});
+
+	return db.save().then(() => newEntry);
 }
 
 function deleteEntry(id) {
 	console.log('deleteEntry');
 	console.log('id: ', id);
+
+	if (!db) throw new Error('Database is not initialized.');
+
+	db.deleteEntry(id);
+
+	return save();
 }
 
 function getAllEntries() {
 	console.log('getAllEntries');
+
+	if (!db) throw new Error('Database is not initialized.');
+
+	return Promise.resolve('Not implemented, but called successfully');
 }
 
 function updateEntry(id, data) {
 	console.log('updateEntry');
 	console.log('id: ', id);
 	console.log('data: ', data);
+
+	if (!db) throw new Error('Database is not initialized.');
 }
 
 function search(keywords) {
 	console.log('search');
 	console.log('keywords: ', keywords);
+
+	if (!db) throw new Error('Database is not initialized.');
 }
 
 function open(databaseFile, databasePassword) {
 	console.log('open');
 	console.log('databaseFile: ', databaseFile);
 	console.log('databasePassword: ', databasePassword);
+
+	db = new Database(databaseFile);
+
+	return db.open(databasePassword);
 }
 
 function close() {
+	if (!db) throw new Error('Database is not initialized.');
+
 	console.log('close');
+
+	db.close();
+
+	return Promise.resolve('Database closed');
 }
 
 module.exports = {
