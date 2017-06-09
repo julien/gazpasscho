2b3a76498063ebd57ab82a84a425ce9817527c63
Carlos Lancha <carlos.lancha@liferay.com>
2017-06-09 16:15:23 -0700

    Massaging getAllEntries response

--- src/pages/Passwords.js
+++ src/pages/Passwords.js
@@ -2,12 +2,31 @@
 
 import Component from 'metal-component';
 import Soy from 'metal-soy';
+const {ipcRenderer} = require('electron');
 
 import templates from './Passwords.soy';
 
 class Passwords extends Component {
+	created() {
+		ipcRenderer.on('allEntriesRetrieved', (event, entries) => {
+			entries = entries.map((entry) => {
+				entry.name = entry.title;
+				return entry;
+			});
+
+			this.entries = entries;
+		});
+
+		ipcRenderer.send('getAllEntries');
+	}
 }
 
 Soy.register(Passwords, templates);
+
+Passwords.STATE = {
+	entries: {
+		value: []
+	}
+}
 
 export default Passwords;
