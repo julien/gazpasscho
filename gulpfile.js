9c9547d503e5ca3cdac6d7e2d741e31e2154ccbe
Iván Zaera Avellón <izaera@gmail.com>
2017-06-09 16:52:17 -0700

    Copy fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 'use strict';
 
+var gulp = require('gulp');
 var metal = require('gulp-metal');
 
 metal.registerTasks({
+	taskPrefix: 'me:',
 	bundleCssFileName: 'gazpasscho.css',
 	bundleFileName: 'gazpasscho.js',
 	moduleName: 'gazpasscho',
+});
+
+gulp.task('build', ['me:build'], () => {
+	gulp
+		.src('node_modules/lexicon-ux/src/fonts/alloy-font-awesome/font/**/*')
+		.pipe(gulp.dest('build'));
 });
