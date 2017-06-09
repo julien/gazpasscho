37ad303761d2b81ee31fba1ca3e39652a42e93a3
drewbrokke <drew.brokke@liferay.com>
2017-06-09 14:04:55 -0700

    ipcHandler.js, api.js - all api methods now log their arguments

--- server/api.js
+++ server/api.js
@@ -1,9 +1,11 @@
 function createEntry(data) {
 	console.log('createEntry');
+	console.log('data: ', data);
 }
 
 function deleteEntry(id) {
 	console.log('deleteEntry');
+	console.log('id: ', id);
 }
 
 function getAllEntries() {
@@ -12,13 +14,17 @@ function getAllEntries() {
 
 function updateEntry(id, data) {
 	console.log('updateEntry');
+	console.log('id: ', id);
+	console.log('data: ', data);
 }
 
 function search(keywords) {
 	console.log('search');
+	console.log('keywords: ', keywords);
 }
 
 function open(databaseFile, databasePassword) {
+	console.log('open: ', open);
 	console.log('databaseFile: ', databaseFile);
 	console.log('databasePassword: ', databasePassword);
 }
--- server/ipcHandler.js
+++ server/ipcHandler.js
@@ -1,30 +1,30 @@
 const ipcMain = require('electron').ipcMain;
 const api = require('./api');
 
-ipcMain.on('createEntry', (event, arg) => {
-	api.createEntry();
+ipcMain.on('createEntry', (event, { data }) => {
+	api.createEntry(data);
 });
 
-ipcMain.on('deleteEntry', (event, arg) => {
-	api.deleteEntry();
+ipcMain.on('deleteEntry', (event, { id }) => {
+	api.deleteEntry(id);
 });
 
-ipcMain.on('getAllEntries', (event, arg) => {
+ipcMain.on('getAllEntries', event => {
 	api.getAllEntries();
 });
 
-ipcMain.on('updateEntry', (event, arg) => {
-	api.updateEntry();
+ipcMain.on('updateEntry', (event, { id, data }) => {
+	api.updateEntry(id, data);
 });
 
-ipcMain.on('search', (event, arg) => {
-	api.search();
+ipcMain.on('search', (event, { keywords }) => {
+	api.search(keywords);
 });
 
 ipcMain.on('open', (event, { databaseFile, databasePassword }) => {
 	api.open(databaseFile, databasePassword);
 });
 
-ipcMain.on('close', (event, arg) => {
+ipcMain.on('close', event => {
 	api.close();
 });
